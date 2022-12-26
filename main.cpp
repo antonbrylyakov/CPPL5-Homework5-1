e6b21dfa28027fc1885cf2cd1a355087f092fcba
Антон Брыляков <yoll@rambler.ru>
2022-12-25 23:47:38 -0800

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include "sqr.hpp"
+
+// вывод вектора на консоль
+template <typename T>
+void printVector(const std::vector<T>& vec)
+{
+	auto first = true;
+	for (const T& v : vec)
+	{
+		if (!first)
+		{
+			std::cout << ", ";
+		}
+		else
+		{
+			first = false;
+		}
+
+		std::cout << v;
+	}
+
+	std::cout << std::endl;
+}
+
+
+int main()
+{
+	int vi = 4;
+	std::cout << "[IN]: " << vi << std::endl;
+	std::cout << "[OUT]: " << sqr(vi) << std::endl;
+
+	double vd = 5.3;
+	std::cout << "[IN]: " << vd << std::endl;
+	std::cout << "[OUT]: " << sqr(vd) << std::endl;
+
+	std::vector vvi{ -1, 4, 8 };
+	std::cout << "[IN]: ";
+	printVector(vvi);
+	std::cout << "[OUT]: ";
+	printVector(sqr(vvi));
+}
---
+++ sqr.hpp
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <vector>
+#include <algorithm>
+
+template <typename T>
+T sqr(const T& src)
+{
+	return src * src;
+}
+
+template <typename T>
+std::vector<T> sqr(const std::vector<T>& src)
+{
+	// поэлементное возведение в квадрат
+	std::vector<T> res(src.size());
+	std::transform(src.begin(), src.end(), res.begin(), [](const T& v) { return v * v; });
+	return res;
+}
